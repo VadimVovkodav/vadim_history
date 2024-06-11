922f77142435e4b72b680c75567d0afba26d3645
VADIM <donaldbiden0000@gmail.com>
2024-06-11 15:13:25 +0300

    3

--- lesson_1.py
+++ lesson_1.py
@@ -1,1 +1,14 @@
-print("hello world")
+number = int(input("введіть п'яти значне число "))
+
+digit5 = number
+digit4 = number
+
+digit3 = number
+
+digit2 = number
+
+digit1 = number
+
+number = digit5, digit4, digit3, digit2, digit1
+
+print()
